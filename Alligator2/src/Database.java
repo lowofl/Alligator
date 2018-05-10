db0747af27f45232f477dd387892f027d8b95a32
Olof Nordengren <olofnordengren@hotmail.com>
2018-05-10 21:34:20 +0200

    Testar vidare

--- Alligator2/src/Database.java
+++ Alligator2/src/Database.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class Database {
     private String name;
     public Database(String fileName) {
-        //testing testing
+        //testing testing 2 22 
         name = "jdbc:sqlite:" + fileName;
 
 
